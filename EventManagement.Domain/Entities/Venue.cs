6e8068c9d1804f5738f5c0bac80d08e512578fa0
Andrea Espinosa <gespinol1210@gmail.com>
2026-05-26 22:55:30 -0500

    Solution created, Event entity added and generated database migration.

---
+++ EventManagement.Domain/Entities/Venue.cs
@@ -0,0 +1,12 @@
+﻿namespace EventManagement.Domain.Entities;
+
+// Representa el lugar donde se realiza el evento
+public class Venue : AuditBase
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    // Relación: un Venue tiene muchos Events
+    public ICollection<Event> Events { get; set; } = new List<Event>();
+}
